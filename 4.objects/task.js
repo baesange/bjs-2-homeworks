eacc475b08cb8959e7e7e076b0b24a7c58804e3d
baesange <145557904+baesange@users.noreply.github.com>
2026-05-04 13:23:33 +0500

    New

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,34 +1,31 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marksToAdd) {
-    if (this.marks) {
-        this.marks.push(...marksToAdd);
-    }
+  if (this.hasOwnProperty('marks')) {
+    this.marks.push(...marksToAdd);
+  }
 }
 
 Student.prototype.getAverage = function () {
-    if (!this.marks || this.marks.length === 0) {
-        return 0;
-    }
-    const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
-    return sum / this.marks.length;
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  
+  const sum = this.marks.reduce((acc, curr) => acc + curr, 0);
+  return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
-}
-
-module.exports = {
-    Student
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
